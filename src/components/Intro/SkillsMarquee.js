21565772d52cd10a097159050cc9b770c21881d0
Kevin R <57162630+kevinrufino@users.noreply.github.com>
2025-01-13 08:25:42 -0500

    fix: push marquee fix

--- src/components/Intro/SkillsMarquee.js
+++ src/components/Intro/SkillsMarquee.js
@@ -1,17 +1,18 @@
-import Marquee from "react-fast-marquee";
 import React from "react";
 
 export const SkillsMarquee = (props) => {
   return (
-    <Marquee className="text-3xl">
-      <p className="font-offbit101Bold m-4">{"skills: "}</p>
-      <p className="font-offbit101 mr-4">
-        {"html, css, javascript, typescript, react, three.js"}
-      </p>
-      <p className="font-offbit101Bold m-4">{"soft skills: "}</p>
-      <p className="font-offbit101 mr-4">
-        {"nerd 🤓, designer, gamer, music producer, sneaker collector"}
-      </p>
-    </Marquee>
+    <marquee className="text-3xl">
+      <div className="flex flex flex-row">
+        <p className="font-offbit101Bold m-4">{"skills: "}</p>
+        <p className="font-offbit101 m-4">
+          {"html, css, javascript, typescript, react, three.js"}
+        </p>
+        <p className="font-offbit101Bold m-4">{"soft skills: "}</p>
+        <p className="font-offbit101 m-4">
+          {"nerd 🤓, designer, gamer, music producer, sneaker collector"}
+        </p>
+      </div>
+    </marquee>
   );
 };
